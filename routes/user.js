5affc93b0eebfb73982607cbc430b0745074c496
007steve <stephen.plummer1.sp@gmail.com>
2021-03-22 13:52:46 -0400

    added user logout login

--- routes/user.js
+++ routes/user.js
@@ -37,5 +37,74 @@ router.post("/register", async (req, res) => {
   }
 });
 
+//Login
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password, photo } = req.body;
+    const user = await User.findOne({ email });
+    //validations
+    if (!user) return res.status(400).send("Invalid email or password.");
+    const passwordCorrect = await bcrypt.compare(password, user.password);
+    if (!passwordCorrect)
+      return res.status(400).send("Invalid email or password.");
+    //JWT
+    const token = jwt.sign(
+      { _id: user._id, name: user.name, photo: user.photo },
+      process.env.JWT_SECRET
+    );
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+      })
+      .send({
+        message: "success",
+        _id: user._id,
+        name: user.name,
+        photo: user.photo,
+      });
+  } catch (error) {
+    console.log(error);
+  }
+});
+// Loggedin
+router.get("/user", async (req, res) => {
+  try {
+    const cookie = req.cookies.token;
+
+    const claims = jwt.verify(cookie, process.env.JWT_SECRET);
+    if (!claims) {
+      return res.status(401).send({
+        message: "unauthenticated",
+      });
+    }
+
+    const user = await User.findOne({ _id: claims._id });
+    const { password, ...data } = await user.toJSON();
+    res.send(data);
+  } catch (error) {
+    res.status(401).send(error.message);
+    console.log(error.message);
+  }
+});
+
+router.get("/loggedin", (req, res) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.json();
+    }
+
+    const validatedUser = jwt.verify(token, process.env.JWT_SECRET);
+    res.send(validatedUser);
+  } catch (error) {
+    return res.json();
+  }
+});
+
+// Logout
+router.get("/logout", (req, res) => {
+  res.clearCookie("token").send();
+});
 
 module.exports = router;
